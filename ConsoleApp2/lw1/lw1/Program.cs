6aaec367446fd99da4108c755db682658c105e1d
Naresh <nareshbarua1@gmail.com>
2020-10-19 11:39:30 +0600

    lw2

---
+++ ConsoleApp2/lw1/lw1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            Console.WriteLine("Even Numbers from 1 to " + 50 + " are: ");
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i + " ");
+                }
+
+            }
+        }
+    }
+}
