cff692a0d2f4ecba96dcd90bcdd3974a8a649357
Naresh <nareshbarua1@gmail.com>
2020-10-19 18:14:14 +0600

    lw5

---
+++ ConsoleApp2/lw5/lw5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("  ");
+                }
+                Console.WriteLine("" + i);
+            }
+        }
+    }
+}
