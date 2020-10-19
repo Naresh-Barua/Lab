75c49bf31f1c5959d9b54b7fd99a3f57c6726d05
Naresh <nareshbarua1@gmail.com>
2020-10-19 18:39:57 +0600

    lw7

---
+++ ConsoleApp2/lw7/lw7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lw7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            for (int k = 4; k >= 0; --k)
+            {
+                for (int j = 1; j <= k; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
