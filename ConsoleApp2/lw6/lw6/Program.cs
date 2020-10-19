12076a5626b70211b303364e105063b457356fc5
Naresh <nareshbarua1@gmail.com>
2020-10-19 18:15:02 +0600

    lw6

---
+++ ConsoleApp2/lw6/lw6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lw6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,digit;
+            int factor = 1;
+            Console.WriteLine("Enter number: ");
+
+            digit = Convert.ToInt32(Console.ReadLine());
+            for (i =1;i<=digit;i++)
+            {
+                factor = factor * i;
+
+            }
+            Console.WriteLine("Factorial is:" + factor);
+
+        }
+    }
+}
