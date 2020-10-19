55edbd137e7531d4ef147046df2d671cafc69daf
Naresh <nareshbarua1@gmail.com>
2020-10-19 17:42:03 +0600

    lw4

---
+++ ConsoleApp2/lw4/lw4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lw4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumEven = 0; ;
+            int sumOdd = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                    sumEven += i;
+                else
+                    sumOdd += i;
+            }
+
+            Console.WriteLine("The sum of even is : " + sumEven);
+            Console.WriteLine("The sum of odd is : " + sumOdd);
+        }
+    }
+}
