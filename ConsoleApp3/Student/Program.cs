afeb234ac54c4c16c57a32243b4771659b6e15e1
Naresh <nareshbarua1@gmail.com>
2020-10-31 10:11:29 +0600

    student

---
+++ ConsoleApp3/Student/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+            stu.Name = "xyz";
+            stu.Id = "19-*****-1";
+            stu.Department = "CSE";
+
+            Console.WriteLine("My name is : " + stu.Name);
+            Console.WriteLine("My id is : " + stu.Id);
+            Console.WriteLine("My department is : " + stu.Department);
+            
+        }
+    }
+    public class Student
+    {
+        private string name;
+        private string id;
+        private string department;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+
+    }
+}
